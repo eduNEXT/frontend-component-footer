a8fba8bfe814d6c5afdc7ecf3aa6c041a97b2b2b
Diana Catalina Olarte <diana.olarte@edunext.co>
2023-11-17 17:05:38 +1100

    feat: remove edx references

--- src/components/studio-footer/StudioFooter.jsx
+++ src/components/studio-footer/StudioFooter.jsx
@@ -29,70 +29,13 @@ const StudioFooter = ({
   // injected
   intl,
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
   const { config } = useContext(AppContext);
 
   return (
     <>
-      <div className="m-0 mt-6 row align-items-center justify-content-center">
-        <div className="col border-top mr-2" />
-        <Button
-          data-testid="helpToggleButton"
-          variant="outline-primary"
-          onClick={() => setIsOpen(!isOpen)}
-          iconBefore={Help}
-          iconAfter={isOpen ? ExpandLess : ExpandMore}
-          size="sm"
-        >
-          {isOpen ? intl.formatMessage(messages.closeHelpButtonLabel)
-            : intl.formatMessage(messages.openHelpButtonLabel)}
-        </Button>
-        <div className="col border-top ml-2" />
-      </div>
+      <div className="m-0 mt-6 row align-items-center justify-content-center col mr-2" />
       <Container size="xl" className="px-4">
-        <TransitionReplace>
-          {isOpen ? (
-            <ActionRow key="help-link-button-row" className="py-4" data-testid="helpButtonRow">
-              <ActionRow.Spacer />
-              <Button as="a" href="https://docs.edx.org/" size="sm">
-                <FormattedMessage {...messages.edxDocumentationButtonLabel} />
-              </Button>
-              <Button
-                as="a"
-                href="https://open.edx.org/"
-                size="sm"
-                data-testid="openEdXPortalButton"
-              >
-                <FormattedMessage {...messages.openEdxPortalButtonLabel} />
-              </Button>
-              <Button
-                as="a"
-                href="https://www.edx.org/course/edx101-overview-of-creating-an-edx-course#.VO4eaLPF-n1"
-                size="sm"
-              >
-                <FormattedMessage {...messages.edx101ButtonLabel} />
-              </Button>
-              <Button
-                as="a"
-                href="https://www.edx.org/course/studiox-creating-a-course-with-edx-studio"
-                size="sm"
-              >
-                <FormattedMessage {...messages.studioXButtonLabel} />
-              </Button>
-              {!_.isEmpty(config.SUPPORT_EMAIL) && (
-                <Button
-                  as="a"
-                  href={`mailto:${config.SUPPORT_EMAIL}`}
-                  size="sm"
-                  data-testid="contactUsButton"
-                >
-                  <FormattedMessage {...messages.contactUsButtonLabel} />
-                </Button>
-              )}
-              <ActionRow.Spacer />
-            </ActionRow>
-          ) : null}
-        </TransitionReplace>
+
         <ActionRow className="pt-3 m-0 x-small">
           © {new Date().getFullYear()} <Hyperlink destination={config.MARKETING_BASE_URL} target="_blank" className="ml-2">{config.SITE_NAME}</Hyperlink>
           <ActionRow.Spacer />
@@ -113,9 +56,8 @@ const StudioFooter = ({
               {intl.formatMessage(messages.accessibilityRequestLinkLabel)}
             </Hyperlink>
           )}
-          <Hyperlink destination={config.LMS_BASE_URL}>LMS</Hyperlink>
         </ActionRow>
-        <ActionRow className="mt-3 pb-4 x-small">
+        <ActionRow className="pb-4 x-small">
           {/*
             Site operators: Please do not remove this paragraph! this attributes back to edX and
               makes your acknowledgement of edX's trademarks clear.
